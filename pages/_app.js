2318f6f71e3a537e19cba1cb42dbae8591051b14
Gökhan Ünal <unal_gokhan@outlook.com>
2022-01-17 22:19:55 +0300

    add ApolloWrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
-
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
+  // console.log('apollo:', apollo);
+  console.log(pageProps);
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
 
-export default MyApp;
+// - give my client but inject apollo also.
+export default withData(MyApp);

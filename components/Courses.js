ae4633321b5b7bd8aa72c49b4832eda3b9a7c184
Gökhan Ünal <unal_gokhan@outlook.com>
2022-01-17 22:32:44 +0300

    add Courses

--- components/Courses.js
+++ components/Courses.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import styled from 'styled-components';
+
+const ALL_COURSES_QUERY = gql`
+  query ALL_COURSES_QUERY {
+    allCourses: courses {
+      id
+      name
+      description
+      status
+      photo {
+        image {
+          publicUrlTransformed
+        }
+        altText
+      }
+    }
+  }
+`;
 
 export default function Courses() {
+  const { loading, error, data } = useQuery(ALL_COURSES_QUERY);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
   return (
-    <div>
-      <h1>Courses Here</h1>
-    </div>
-  )
+    <CourseList>
+      {data.allCourses.map((course) => (
+        <div key={course.id}>
+          <p>{course.name}</p>
+        </div>
+      ))}
+    </CourseList>
+  );
 }
+
+const CourseList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 2rem;
+`;

f59b347ce444305977470c45d9e42fa15f245e07
Gökhan Ünal <unal_gokhan@outlook.com>
2022-01-17 22:19:36 +0300

    fix serverside stylesheet

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
+  // wait until this resolve before sending data to server into client
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
+
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
   render() {
     return (
       <Html>

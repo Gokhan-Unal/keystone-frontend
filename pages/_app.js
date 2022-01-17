aed899e18bb96af99fd3d84ef83f6e2a9cd9ad5d
Gökhan Ünal <unal_gokhan@outlook.com>
2022-01-18 00:46:53 +0300

    we add this bc we want to get id's variables etc.

--- pages/_app.js
+++ pages/_app.js
@@ -14,5 +14,20 @@ function MyApp({ Component, pageProps, apollo }) {
   );
 }
 
+// - go and fetch all of the queries that are in all of the children components
+// -for id's and etc.
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  console.log('pageProps1', pageProps);
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+    console.log('pageProps2', pageProps);
+  }
+  console.log('ctx is important <3', ctx);
+  pageProps.query = ctx.query; // - get any query variables /products /to /id etc.
+  console.log('pageProps3', pageProps);
+  return { pageProps };
+};
+
 // - give my client but inject apollo also.
 export default withData(MyApp);

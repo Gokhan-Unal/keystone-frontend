5e3650736c33b4391933c425807ca494be63e9a4
Gökhan Ünal <unal_gokhan@outlook.com>
2022-01-17 16:16:12 +0300

    Global Styles and styles

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import Link from 'next/link';
 import Nav from './Nav';
+import styled from 'styled-components';
+
+const Logo = styled.div`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: skyblue;
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .main {
+    display: flex;
+    gap: 1rem;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 2rem;
+  }
+  .sub-main {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid black;
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
-      <div>
-        <Link href="/">Teach</Link>
+    <HeaderStyles>
+      <div className="main">
+        <Logo>
+          <Link href="/">Teach</Link>
+        </Logo>
+        <Nav />
       </div>
-      <div>
+      <div className="sub-main">
         <p>Search</p>
       </div>
-      <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
--- components/Page.js
+++ components/Page.js
@@ -1,5 +1,43 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    --blue: skyblue;
+    --black: '#393939';
+    --gray: '#3A3A3A';
+    --max-width: 1100px;
+    --bs: 0 12px 24px 0 rgba(0, 0, 0, 0.09);
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  }
+  a {
+    text-decoration: none;
+    color: var(--black);
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--max-width);
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 /**
  * @description wrapper
@@ -7,8 +45,9 @@ import Header from './Header';
 export default function Page({ children }) {
   return (
     <>
+      <GlobalStyles />
       <Header />
-      {children}
+      <InnerStyles>{children}</InnerStyles>
     </>
   );
 }

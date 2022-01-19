257f253157004164d06b45db1fd6fff04cc7cff8
Gökhan Ünal <unal_gokhan@outlook.com>
2022-01-20 00:15:05 +0300

    renaming

--- pages/course.js
+++ pages/course.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import Courses from '../components/Courses'
+import React from 'react';
+import Courses from '../components/Courses';
 
 export default function CoursePage() {
   return (
     <div>
       <Courses />
     </div>
-  )
+  );
 }
